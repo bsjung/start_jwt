191e5a3b3b8d050d2939b3461eafca6c29ccb0e4
Benjamin Jung <bsjung@gmail.com>
2020-02-01 02:25:41 +0900

    Update Start JWT  - add examples

---
+++ examples/sample.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+import 'package:start_jwt/json_web_token.dart';
+
+void main() {
+
+    // Encode (i.e. sign) a payload into a JWT token.
+    
+    final jwt = new JsonWebTokenCodec(secret: "My secret key");
+    final payload = {
+      'iss': 'joe',
+      'exp': 1300819380,
+      'http://example.com/is_root': true
+    };
+    final token = jwt.encode(payload);
+    print ("Payload : " + payload.toString());
+    
+    // Validate a token.
+    
+    jwt.isValid(token);
+    
+    // Decode (i.e. extract) the payload from a JWT token.
+    
+    final decoded = jwt.decode(token);
+    print ("Decoded : " + decoded.toString());
+}
+
